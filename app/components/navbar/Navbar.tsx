cb1f317d2f3d9d443690b626fff93fe6dd58581b
RyunosukeSakurada <longzhizhu513@gmail.com>
2023-10-14 12:35:13 -0700

    Navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Container from '../Container'
 import Logo from './Logo'
+import UseMenu from './UseMenu'
 
 const Navbar = () => {
   return (
     <div className='fixed w-full bg-white z-10 shadow-md'>
       <div className='p-4 border-b'>
         <Container>
-          <Logo />
+          <div className='flex flex-row justify-between items-center'>
+            <Logo />
+            <UseMenu />
+          </div>
         </Container>
       </div>
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <div>
+        <div className='p-24'>
           {children}
         </div>
       </body>
